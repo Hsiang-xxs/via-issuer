cc2791c8b1514bc7a72b8ccd53098cb892df9fa0
Kallol Borah <18071236+kallolborah@users.noreply.github.com>
2020-06-14 23:47:38 +0530

    refactored via contract

--- contracts/Via.sol
+++ contracts/Via.sol
@@ -2,91 +2,211 @@
 
 pragma solidity >=0.4.16 <0.7.0;
 
-contract Via{
+contract Issuer{
 
-    //address of the issuer of the Via
-    address public issuer;
+    using SafeMath for uint256;
 
-    //is issuer set up complete ?
-    bool setup;
+    //this issuer's address
+    address owner;
+
+    //instance variables for Via tokens
+    Token ViaUSD;
+    Token ViaEUR;
+    Token ViaINR;
+
+    //structure to keep track of deposits
+    struct Deposit{
+        uint256 value;
+        bytes32 currency;
+    }
+
+    //each depositor may have multiple currencies, hence an array
+    mapping(address => Deposit[]) public depositors;
+    
+    //constructor that assigns the issuer
+    constructor() public { 
+        owner = msg.sender;
+        //create Via token contracts
+        ViaUSD = new Token("Via-USD");
+        ViaEUR = new Token("Via-EUR");
+        ViaINR = new Token("Via-INR");
+    }
+   
+    //receive ether and store it against sender address
+    receive() external payable{
+        //issuer can not deposit 
+        require(msg.sender != owner);
+        //there should be some deposit
+        require(msg.value != 0);
+        //store received ether
+        //check if ether as a currency is already available in the deposits array
+        bool found=false; 
+        uint p=0;
+        for(p=0; p<depositors[msg.sender].length; p++){
+            if(depositors[msg.sender][p].currency="ether"){
+                depositors[msg.sender][p].value += msg.value;
+                found = true;
+            }
+        }
+        //if not found, add ether as currency and add what is sent by the depositor
+        if(!found){
+            depositors[msg.sender][p].currency = "ether";
+            depositors[msg.sender][p].value = msg.value;
+        }
+    }
+
+    //issue Via tokens for currency against deposit
+    function issue(bytes32 currency) public returns(bool){
+        //issuer can not issue to itself
+        require(msg.sender != owner);
+        //amount of ether to issue via against can not be zero
+        require(msg.value != 0);
+        //the requestor should have some deposits
+        require(depositors[msg.sender].length !=0);
+        //call Oracle to check exchange rate of ether/other currency deposit with the fiat currency in request param
+        //for the time being assuming it is 1:1
+        bool issued = false;
+        uint p=0;
+        for(p=0; p<depositors[msg.sender].length; p++){
+            //call oracle to find exchange rate of depositors[msg.sender][p].currency against currency passed in param
+            //msg.value should be replaced with amount after applying exchange rate
+            if(depositors[msg.sender][p].value >= msg.value){ 
+                if(currency == "Via-USD"){
+                    ViaUSD.issue(msg.value, msg.sender);
+                    issued = true;
+                }
+                if(currency == "Via-EUR"){
+                    ViaEUR.issue(msg.value, msg.sender);
+                    issued = true;
+                }
+                if(currency == "Via-INR"){
+                    ViaINR.issue(msg.value, msg.sender);
+                    issued = true;
+                }
+            }
+        }
+    }
+
+    //redeem issued Via tokens
+    function redeem() public payable{
+        require(msg.sender != owner);
+        require(msg.value != 0);
+        //to do : integrate to bank's credit initiation API
+    }
+
+    //keep Via tokens in custody
+    function deposit() public payable{
+        require(msg.sender != owner);
+        require(msg.value != 0);
+        //to do : integrate to institutional custody
+    }
+
+}
+
+contract ERC20{
+
+    //variables
+    uint256 totalSupply_;
+
+    //Via balances held by this address
+    mapping(address => uint256) public balances;
+    //Delegates allowed to access this address
+    mapping(address => mapping (address => uint256)) allowed;
+
+    //erc20 standard events
+    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+    event Transfer(address indexed from, address indexed to, uint tokens);
+
+    //erc20 standard functions
+    function totalSupply() public view returns (uint256){
+        return totalSupply_;
+    }
+
+    function balanceOf(address tokenOwner) public view returns (uint){
+        return balances[tokenOwner];
+    }
+
+    function transfer(address receiver, uint tokens) public returns (bool){
+        require(tokens <= balances[msg.sender]);
+        balances[msg.sender] = balances[msg.sender].sub(tokens);
+        balances[receiver] = balances[receiver].add(tokens);
+        emit Transfer(msg.sender, receiver, tokens);
+        return true;
+    }
+
+    function approve(address spender, uint tokens)  public returns (bool){
+        allowed[msg.sender][spender] = tokens;
+        emit Approval(msg.sender, spender, tokens);
+        return true;
+    }
+
+    function allowance(address tokenOwner, address spender) public view returns (uint){
+        return allowed[tokenOwner][spender];
+    }
+
+    function transferFrom(address owner, address buyer, uint tokens) public returns (bool){
+        require(tokens <= balances[owner]);
+        require(tokens <= allowed[owner][msg.sender]);
+        balances[owner] = balances[owner].sub(tokens);
+        allowed[owner][msg.sender] = allowed[owner][msg.sender].sub(tokens);
+        balances[buyer] = balances[buyer].add(tokens);
+        Transfer(owner, buyer, tokens);
+        return true;
+    }
+
+}
+
+
+contract Token is ERC20{
+
+    //address of the issuer of the Via, set once, never reset again
+    address immutable issuer;
+
+    //name of Via token (eg, Via-USD)
+    string public immutable name;
+    string public immutable symbol;
+    
 
     //a Via has balance value equal to a fiat currency
     //for example, 1 Via-USD corresponds to 1 Via corresponding to the USD fiat
     struct Via{
-        uint value;
+        uint256 value;
         bytes32 currency;
     }
 
-    Via[] public holdings;
-
-    //a Via loan has some value, corresponds to a fiat currency
-    //has a borrower and lender that have agreed to a zero coupon rate called price
-    //and a tenure in unix timestamps of seconds counted from 1970-01-01
-    //the boolean offer indicates if the Via loan is an offer to lend or not
-    struct ViaLoans{
-        uint value;
-        bytes32 currency;
-        address payable borrower;
-        address payable lender;
-        uint tenure;
-        uint price;
-        bool offer;
-    }
-
-    ViaLoans[] public loans;
-
-    //data structure to hold offers to lend Via in a specific fiat currency
-    //over a floor price and floor rate and over a minimum tenure
-    struct ViaOffers{
-        uint value;
-        bytes32 currency;
-        uint tenure;
-        uint price;
-    }
-
-    ViaOffers[] public offers;
-
-    //any Via user can have some holdings and some loans
-    struct Beneficiary{
-        uint holdings;
-        uint loans;
-        uint offers;
-    }
-    
-    //declares a state variable for each beneficiary
-    mapping(address => Beneficiary) public beneficiaries;
+    mapping(address => Via[]) public holdings;
 
     //events to capture and report to Via oracle
     event sold(bytes32 currency, uint value);
-    event lent(bytes32 currency, uint value, uint tenure, uint price);
 
-    //constructor that assigns the issuer
-    constructor() public {
-        issuer = msg.sender;
-        setup = true;
+    //constructor for creating Via token
+    constructor(bytes32 tokenName) public returns (address){
+        //setting issuer address and via token name
+        issuer = msg.sender;   
+        name = tokenName;
+        symbol = tokenName;     
     }
 
     //requesting issue of Via for value and for fiat currency
-    function buy(bytes32 currency) payable public {
-        //ensure that the issuer is not buying and that issuer setup is done
-        require(msg.sender != issuer && setup);
+    function issue(uint256 amount, address buyer) public {
+        //ensure that request is sent by the issuer
+        require(msg.sender == issuer);
         //ensure that brought amount is not zero
-        require(msg.value != 0);
-        //add via corresponding to currency (eg, ether or fiat or some other via-fiat) in beneficiary holdings
-        bytes32 name;
-        holdings[holdings.length].currency = name;
-        //find Via exchange rate for currency from via oracle if currency is not ether, else first find value of ether in fiat
-        uint exchanged;
-        //add via exchanged for amount of currency to beneficiary holdings 
-        holdings[holdings.length].value = exchanged;
+        require(amount != 0);
+        //add amount in issuer/sender's balances
+        super.balances[msg.sender] += amount;
+        //transfer amount from issuer/sender to buyer 
+        super.transfer(buyer, amount);
+        //adjust total supply
+        super.totalSupply_ += amount;
         //generate event
-        emit sold(name, exchanged);
+        emit sold(name, amount);
     }
 
     //requesting redemption of Via for value and for fiat currency
     function sell(bytes32 currency) payable public {
         //ensure that the issuer is not selling and that issuer setup is done
-        require(msg.sender != issuer && setup);
+        require(msg.sender != issuer);
         //ensure that sold amount is not zero
         require(msg.value != 0);
         //remove via corresponding to currency (eg, ether or fiat or some other via-fiat) in beneficiary holdings
@@ -96,10 +216,47 @@ contract Via{
         uint exchanged;
         //subtract via exchanged for amount of currency to beneficiary holdings 
         holdings[holdings.length].value = exchanged;
+        //adjust total supply
+        totalSupply_ -= exchanged;
         //generate event
         emit sold(name, exchanged);
     }
 
+}
+
+
+contract Loan is ERC20{
+
+    //a Via loan has some value, corresponds to a fiat currency
+    //has a borrower and lender that have agreed to a zero coupon rate called price
+    //and a tenure in unix timestamps of seconds counted from 1970-01-01
+    //the boolean offer indicates if the Via loan is an offer to lend or not
+    struct ViaLoans{
+        uint256 value;
+        bytes32 currency;
+        address payable borrower;
+        address payable lender;
+        uint tenure;
+        uint price;
+        bool offer;
+    }
+
+    mapping(address => ViaLoans[]) public loans;
+
+    //data structure to hold offers to lend Via in a specific fiat currency
+    //over a floor price and floor rate and over a minimum tenure
+    struct ViaOffers{
+        uint256 value;
+        bytes32 currency;
+        uint tenure;
+        uint price;
+    }
+
+    mapping(address => ViaOffers[]) public offers;
+
+    //events to capture and report to Via oracle
+    event lent(bytes32 currency, uint value, uint tenure, uint price);
+  
     //offering a loan in a currency that is of most benefit
     function offer(bytes32 fiat, uint value) payable public {
 
@@ -108,7 +265,7 @@ contract Via{
     //bid to borrow on an offer
     function bid(bytes32 currency, uint price, uint tenure, uint amount) payable public {
         //ensure that the issuer is not borrowing and that issuer setup is done
-        require(msg.sender != issuer && setup);
+        require(msg.sender != issuer);
         //ensure that collateral offered is not zero
         require(msg.value != 0);
         //ensure that there is currency and enough of it to lend
@@ -123,5 +280,23 @@ contract Via{
     function payback() payable public {
 
     }
+
+
+}
+
+library SafeMath { // Only relevant functions
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    assert(b <= a);
+    return a - b;
+    }
+    function add(uint256 a, uint256 b) internal pure returns (uint256)   {
+    uint256 c = a + b;
+    assert(c >= a);
+    return c;
+    }
+}
+
+library ViaOracle{
+
 
 }
